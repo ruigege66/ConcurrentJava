a01575172a2081ae685f9421970d0d26239d21f7
ruigege66 <1215217867@qq.com>
2020-10-30 23:53:06 +0800

    源码级别interrupted()方法讲解

---
+++ ConcurrentJava/src/com/ruigege/threadFoundation1/isInterruptedAndinterrupted.java
@@ -0,0 +1,23 @@
+package com.ruigege.threadFoundation1;
+
+public class isInterruptedAndinterrupted {
+	public static void main(String[] args) throws InterruptedException {
+		Thread threadOne = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				for(;;) {
+					
+				}
+			}
+		});
+		
+		threadOne.start();
+		threadOne.interrupt();
+		
+		System.out.println("子线程isInterrupted:"+threadOne.isInterrupted());
+		System.out.println("子线程Interrupted:"+threadOne.interrupted());
+		System.out.println("主线程Interrupted:"+Thread.interrupted());
+		System.out.println("子线程isInterrupted:"+threadOne.isInterrupted());
+		threadOne.join();
+	}
+}
