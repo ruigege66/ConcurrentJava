d9b9e89f54b4100fb2f47ae2b256db07116d1799
ruigege66 <1215217867@qq.com>
2020-11-05 00:07:55 +0800

    解除死锁

--- ConcurrentJava/src/com/ruigege/threadFoundation1/DeadLockExample.java
+++ ConcurrentJava/src/com/ruigege/threadFoundation1/DeadLockExample.java
@@ -1,5 +1,53 @@
 package com.ruigege.threadFoundation1;
 
 public class DeadLockExample {
+	//创建资源
+	private static Object object1 = new Object();
+	private static Object object2 = new Object();
 
+	public static void main(String[] args) throws InterruptedException {
+		Thread thread1 = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				synchronized(object1) {
+					System.out.println("线程1获得了资源1");
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					synchronized(object2) {
+						System.out.println("线程1获得了资源2");
+					}
+					
+				}
+
+			}
+		});
+		Thread thread2 = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				synchronized(object1) {
+					System.out.println("线程2获得了资源2");
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					synchronized(object2) {
+						System.out.println("线程2获得了资源1");
+					}					
+				}
+
+			}
+		});
+		thread1.start();
+		thread2.start();
+		thread1.join();
+		thread2.join();
+		
+		
+	}
 }
