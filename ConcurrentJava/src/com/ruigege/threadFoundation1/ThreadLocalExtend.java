b24400a0c7c4c2aba7a4f6ad2bc9f3d326a2bdfe
ruigege66 <1215217867@qq.com>
2020-11-10 23:44:20 +0800

    提交ThreadLocal不具有继承性的代码

---
+++ ConcurrentJava/src/com/ruigege/threadFoundation1/ThreadLocalExtend.java
@@ -0,0 +1,25 @@
+package com.ruigege.threadFoundation1;
+
+public class ThreadLocalExtend {
+
+	public static ThreadLocal<String> threadLocal = new ThreadLocal<>();
+	
+	public static void main(String[] args) {
+		Thread thread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				threadLocal.set("我是子线程的");
+				System.out.println(threadLocal.get());
+			}
+		});
+		
+		thread.start();
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(threadLocal.get());
+	}	
+}
