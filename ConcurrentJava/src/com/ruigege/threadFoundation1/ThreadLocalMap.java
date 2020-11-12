0d206503e3e8acf355a3a9bbf534c92adaa9f9fd
ruigege66 <1215217867@qq.com>
2020-11-13 00:12:27 +0800

    编写ThreadLocalMap的构造方法代码

--- ConcurrentJava/src/com/ruigege/threadFoundation1/ThreadLocalMap.java
+++ ConcurrentJava/src/com/ruigege/threadFoundation1/ThreadLocalMap.java
@@ -1,5 +1,35 @@
 package com.ruigege.threadFoundation1;
 
 public class ThreadLocalMap {
-
+	
+	private Entry[] table;
+	private T 
+	
+	private ThreadLocalMap(ThreadLocalMap parentMap) {
+		Entry[] parentTable = parentMap.table;
+		int len = parentTable.length;
+		setThreshole(len);
+		table = new Entry[len];
+		
+		
+		for(int j=0;j<len;j++) {
+			Entry e = parentTable[j];
+			if(e != null) {
+				//下面这几行就是为了获取Entry实力的key和value并且生成一个新的Entry实例
+				@SuppressWarnings("unchecked")
+				ThreadLocal<Object> key = (ThreadLocal<Object>)e.get();
+				if(key != null) {
+					Object value = key.chirld(e.value);//返回e.value
+					Entry c = new Entry(key,value);
+					int h = key.threadLocalHashCode & (len-1);
+					while(table[h] != null) {
+						h = nextIndex(h,len);
+					}
+					table[h] = c;
+					size++;
+				}
+			}
+		}
+	}
+	
 }
