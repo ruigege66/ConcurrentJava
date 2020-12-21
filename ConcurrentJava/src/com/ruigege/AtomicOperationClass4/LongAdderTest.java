585a02f06b256080c8e89c83abf74e255b57dfd6
ruigege66 <1215217867@qq.com>
2020-12-22 00:07:53 +0800

    提交LongAdder源码

--- ConcurrentJava/src/com/ruigege/AtomicOperationClass4/LongAdderTest.java
+++ ConcurrentJava/src/com/ruigege/AtomicOperationClass4/LongAdderTest.java
@@ -1,7 +1,7 @@
 package com.ruigege.AtomicOperationClass4;
 import sun.misc.Unsafe;
 
-public class LongAdderTest {
+@sun.misc.Contended public class LongAdderTest {
 	
 	volatile long value;
 	
@@ -14,7 +14,38 @@ public class LongAdderTest {
 	static {
 		try {
 			unsafe = Unsafe.getUnsafe();
-			Class<?>
+			Class<?> ak = Cell.class;
+			valueOffset = unsafe.objectFieldOffset(ak.getDeclaredField("value"));
+		}catch(Exception e) {
+			throw new Error(e);
+		}
+	}
+	
+	final long cas(long cmp,long val) {
+		return unsafe.compareAndSwapLong(this,valueOffset,cmp,val);
+	}
+	
+	public long sum() {
+		Cell[] as = cells;
+		long sum = base;
+		if(as != null) {
+			for (int i=0;i<as.length;i++) {
+				if(Cell[i] != null) {
+					sum += Cell[i].value;
+				}
+			}
+		}
+	}
+	
+	public void reset() {
+		Cell[] as = cells;
+		base = 0L;
+		if(as != null) {
+			for (int i=0;i<as.length;i++) {
+				if(as[i] != null) {
+					as[i].value = 0L;
+				}
+			}
 		}
 	}
 }
