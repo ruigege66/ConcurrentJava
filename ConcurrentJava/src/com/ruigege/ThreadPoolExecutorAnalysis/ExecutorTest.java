7da5abeed10ce105284a8384dc5dc57f370b05e8
ruigege66 <1215217867@qq.com>
2021-07-29 00:17:53 +0800

    线程池的两种创建方式

--- ConcurrentJava/src/com/ruigege/ThreadPoolExecutorAnalysis/ExecutorTest.java
+++ ConcurrentJava/src/com/ruigege/ThreadPoolExecutorAnalysis/ExecutorTest.java
@@ -1,5 +1,22 @@
 package com.ruigege.ThreadPoolExecutorAnalysis;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 public class ExecutorTest {
+	public static ExecutorService newFixeThreadPool (int nThreads) {
+		return new ThreadPoolExecutor(nThreads, nThreads, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>());
+	}
+	
+	public static ExecutorService newFixeThreadPool (int nThreads, ThreadFactory threadFactory) {
+		return new ThreadPoolExecutor(nThreads, nThreads, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>(), threadFactory);
+	}
+	
+	public static ExecutorService newSingleThreadExecutor() {
+		return new FinalizableDelegatedExecutorService(new ThreadPoolExecutor(1, 1, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>()));
+	}
 
 }
