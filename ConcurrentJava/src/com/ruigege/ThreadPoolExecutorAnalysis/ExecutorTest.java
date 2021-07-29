3f75086f60748e68ac93d5e6263f7b7fb0324a97
ruigege66 <1215217867@qq.com>
2021-07-30 00:07:12 +0800

    wdfsdf th '#' will be ignored, and an empty message aborts the commit.

--- ConcurrentJava/src/com/ruigege/ThreadPoolExecutorAnalysis/ExecutorTest.java
+++ ConcurrentJava/src/com/ruigege/ThreadPoolExecutorAnalysis/ExecutorTest.java
@@ -2,6 +2,7 @@ package com.ruigege.ThreadPoolExecutorAnalysis;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
@@ -14,9 +15,24 @@ public class ExecutorTest {
 	public static ExecutorService newFixeThreadPool (int nThreads, ThreadFactory threadFactory) {
 		return new ThreadPoolExecutor(nThreads, nThreads, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>(), threadFactory);
 	}
+
 	
 	public static ExecutorService newSingleThreadExecutor() {
 		return new FinalizableDelegatedExecutorService(new ThreadPoolExecutor(1, 1, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>()));
+	}
+	
+	// 使用自己的工厂
+	public static ExecutorService newSingleThreadExecutor(ThreadFactory threadFactory) {
+		return new FinalizableDelegatedExecutorService(new ThreadPoolExecutor(1, 1, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>()), threadFactory);
+	}
+	
+	public static ExcecutorService newCachedThreadPool() {
+		return new ThreadPoolExecutor(0, Integer.MAX_VALUE, 60L,TimeUnit.SECONDS, new SynchronousQueue<Runnable>());
+	}
+	
+	// 使用自定义的线程工厂
+	public static ExcecutorService newCachedThreadPool(ThreadFactory threadFactory) {
+		return new ThreadPoolExecutor(0, Integer.MAX_VALUE, 60L,TimeUnit.SECONDS, new SynchronousQueue<Runnable>(), threadFactory);
 	}
 
 }
