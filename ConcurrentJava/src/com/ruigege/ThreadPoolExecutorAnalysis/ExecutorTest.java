0f068736b8cfa13ebf73940126715555108c7c65
ruigege66 <1215217867@qq.com>
2021-08-03 00:40:15 +0800

    修改内存共享机制

--- ConcurrentJava/src/com/ruigege/ThreadPoolExecutorAnalysis/ExecutorTest.java
+++ ConcurrentJava/src/com/ruigege/ThreadPoolExecutorAnalysis/ExecutorTest.java
@@ -16,7 +16,6 @@ public class ExecutorTest {
 		return new ThreadPoolExecutor(nThreads, nThreads, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>(), threadFactory);
 	}
 	
-
 	
 	public static ExecutorService newSingleThreadExecutor() {
 		return new FinalizableDelegatedExecutorService(new ThreadPoolExecutor(1, 1, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>()));
